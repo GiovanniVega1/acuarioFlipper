8ee410d4377dd6270d2658477e7958230a593393
GiovanniVega1 <edwinvega.torres@gmail.com>
2023-11-27 23:16:26 -0600

    servicios

--- src/components/carrusel/ExampleCarouselImage.jsx
+++ src/components/carrusel/ExampleCarouselImage.jsx
@@ -7,14 +7,14 @@ export function UncontrolledExample() {
       <Carousel.Item>
       <img
           className="d-block w-50 img-responsive"
-          src="imgs/acuario.jpg"
+          src="imgs/submarino.jpg"
           alt="First slide"
         />
         <Carousel.Caption>
           <div className="d-flex justify-content-end align-items-center">
-            <div className="text-center">
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            <div>
+              <h3>Paseo Submarino</h3>
+              <p>Nuestro compromiso es brindar una experiencia completa para toda la familia.</p>
             </div>
           </div>
         </Carousel.Caption>
@@ -22,14 +22,14 @@ export function UncontrolledExample() {
       <Carousel.Item>
         <img
           className="d-block w-50 img-responsive"
-          src="imgs/acuario.jpg"
+          src="imgs/tiburones.jpg"
           alt="Second slide"
         />
         <Carousel.Caption>
          <div className="d-flex justify-content-end align-items-center">
-            <div className="text-center">
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            <div>
+              <h3>Nadar con Tiburones</h3>
+              <p>Una actividad que seguramente te dejará con recuerdos inolvidables.</p>
             </div>
           </div>
         </Carousel.Caption>
@@ -37,14 +37,14 @@ export function UncontrolledExample() {
       <Carousel.Item>
       <img
           className="d-block w-50 img-responsive"
-          src="imgs/acuario.jpg"
+          src="imgs/tortuga.jpg"
           alt="Third slide"
         />
         <Carousel.Caption>
           <div className="d-flex justify-content-end align-items-center">
-            <div className="text-center">
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            <div>
+              <h3>Sostenibilidad y la conservación marina</h3>
+              <p>Ofrecemos programas de adopción de tortugas marinas.</p>
             </div>
           </div>
         </Carousel.Caption>
